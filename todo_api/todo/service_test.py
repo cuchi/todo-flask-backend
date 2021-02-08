5829c527e075f36daa8040bf2161e173480a3412
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2021-02-08 00:19:59 -0300

    chore: fix lint issue

--- todo_api/todo/service_test.py
+++ todo_api/todo/service_test.py
@@ -40,16 +40,17 @@ def test_late_todos(db_session, faker):
         service.create(name=faker.text(), due_at=now - one_hour)
     for _ in range(3):
         service.create(name=faker.text(), due_at=now + one_hour)
-        
+
     assert len(service.all()) == 9
     assert len(service.all(late=True)) == 3
     assert len(service.all(late=False)) == 6
+
 
 def test_name_search(db_session):
     service.create(name="Do something")
     service.create(name="Do something again")
     service.create(name="Do the other thing")
-    
+
     result = service.all(name="something")
-    
+
     assert len(result) == 2
